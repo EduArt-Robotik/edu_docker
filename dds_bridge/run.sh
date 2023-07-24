656b4637c5000bde1df68b6961033c5ec35caf86
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-07-24 13:34:32 +0200

    Add test topic publishing to dds bridge.

--- dds_bridge/run.sh
+++ dds_bridge/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-source /opt/ros/humble/setup.bash
-ros2 daemon start
+source /opt/ros/humble/setup.bash && ros2 daemon start && ros2 topic pub /dds_bridge std_msgs/msg/String "{data: 'sent from dds bridge'}"
 
 while true
 do
