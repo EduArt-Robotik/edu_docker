a8a854803c00b70aff1f6e5d7a8ca5c12ae8176d
Christian Wendt <christian.wendt@eduart-robotik.com>
2023-11-15 15:13:52 +0100

    Switch to namespace set by environment variable.

--- dds_bridge/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /opt/ros/humble/setup.bash && ros2 daemon start && ros2 topic pub /dds_bridge std_msgs/msg/String "{data: 'sent from dds bridge'}"
-
-while true
-do
-  sleep 1000
-done
---
+++ rplidar/launch_content/rplidar.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, EnvironmentVariable
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,  # A1 / A2
+                # 'serial_baudrate': 256000, # A3
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True,
+                'topic_name': 'scan'
+            }],
+
+            namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
+        ),
+    ])
