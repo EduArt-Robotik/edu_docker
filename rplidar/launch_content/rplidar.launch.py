6314d872b6d70e1fafdf9383d2a3035e8a1ba630
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-02-19 14:13:21 +0100

    Add launch and parameter files for navigation.

--- rplidar/launch_content/rplidar.launch.py
+++ rplidar/launch_content/rplidar.launch.py
@@ -1,24 +1,36 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, EnvironmentVariable
+from launch.substitutions import Command, EnvironmentVariable, PathJoinSubstitution
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            name='rplidar_composition',
-            package='rplidar_ros',
-            executable='rplidar_composition',
-            output='screen',
-            parameters=[{
-                'serial_port': '/dev/ttyUSB0',
-                'serial_baudrate': 115200,  # A1 / A2
-                # 'serial_baudrate': 256000, # A3
-                'frame_id': 'laser',
-                'inverted': False,
-                'angle_compensate': True,
-                'topic_name': 'scan'
-            }],
+    robot_namespace = EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
 
-            namespace=EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
-        ),
+    rplidar_node = Node(
+        name='rplidar_composition',
+        package='rplidar_ros',
+        executable='rplidar_composition',
+        output='screen',
+        parameters=[{
+            'serial_port': '/dev/ttyUSB0',
+            'serial_baudrate': 115200,  # A1 / A2
+            # 'serial_baudrate': 256000, # A3
+            'frame_id': PathJoinSubstitution([robot_namespace, 'laser']),
+            'inverted': False,
+            'angle_compensate': True,
+            'topic_name': 'scan'
+        }],
+        namespace=robot_namespace
+    )
+    tf_laser = Node(
+      package='tf2_ros',
+      executable='static_transform_publisher',
+      arguments=[
+        '0.11', '0.0', '0.125', '0', '0', '0',
+        PathJoinSubstitution([robot_namespace, 'base_link']),
+        PathJoinSubstitution([robot_namespace, 'laser'])
+      ]
+    )
+    return LaunchDescription([
+        rplidar_node,
+        tf_laser
     ])
