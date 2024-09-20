82c29f8512d3d763b8ae996672ed5462bef94a2e
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-09-20 12:28:31 +0200

    fix slam toolbox launch file

--- slam_tool_box/launch_content/slam.launch.py
+++ slam_tool_box/launch_content/slam.launch.py
@@ -2,7 +2,7 @@ import os
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, ParameterFile
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from nav2_common.launch import RewrittenYaml
