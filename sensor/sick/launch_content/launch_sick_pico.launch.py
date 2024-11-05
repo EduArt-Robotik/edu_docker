1fc5f54caa05d412633e3e2d17d1630d8e808bfd
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-11-05 14:43:24 +0100

    Add launch file for sick lidars.

---
+++ sensor/sick/launch_content/launch_sick_pico.launch.py
@@ -0,0 +1,35 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, EnvironmentVariable, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+  # Launch File Arguments
+  edu_robot_namespace = LaunchConfiguration('edu_robot_namespace')
+  edu_robot_namespace_arg = DeclareLaunchArgument(
+    'edu_robot_namespace', default_value=os.getenv('EDU_ROBOT_NAMESPACE', default='eduard')
+  )
+
+  # Launching needed processes for localization
+  sick_scan_launch_file = PathJoinSubstitution([
+    FindPackageShare('sick_scan_xd'),
+    'launch',
+    'sick_tim_7xx.launch.py'
+  ])
+  sick_scan = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(sick_scan_launch_file),
+    launch_arguments={
+      'hostname': '192.168.0.70',
+    }.items()
+  )
+
+  return LaunchDescription([
+    edu_robot_namespace_arg,
+    sick_scan
+  ])
+  
