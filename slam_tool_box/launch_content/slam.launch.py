39f91dbd8234c906b9d883616e4834e33f430556
Christian Wendt <christian.wendt@eduart-robotik.com>
2024-12-18 12:42:33 +0100

    adapt launch file for slam tool box after switching to ROS jazzy.

--- slam_tool_box/launch_content/slam.launch.py
+++ slam_tool_box/launch_content/slam.launch.py
@@ -1,13 +1,22 @@
 import os
  
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, EmitEvent, LogInfo, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration, EnvironmentVariable
-from launch_ros.actions import Node
+from launch_ros.actions import Node, LifecycleNode
 from ament_index_python.packages import get_package_share_directory
 from nav2_common.launch import RewrittenYaml
- 
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
+from launch.conditions import IfCondition
+from launch.events import matches_action
+from launch.substitutions import (AndSubstitution, LaunchConfiguration,
+                                  NotSubstitution)
+
 def generate_launch_description():
+    autostart = LaunchConfiguration('autostart')
+    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")    
     use_sim_time = LaunchConfiguration('use_sim_time')
     robot_namespace = os.environ.get('EDU_ROBOT_NAMESPACE', '/eduard/')
     if len(robot_namespace) == 0: 
@@ -31,7 +40,15 @@ def generate_launch_description():
         default_value=os.path.join(get_package_share_directory("slam_toolbox"),
                                    'config', 'mapper_params_online_sync.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
- 
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the slamtoolbox. '
+                    'Ignored when use_lifecycle_manager is true.')
+    declare_use_lifecycle_manager = DeclareLaunchArgument(
+        'use_lifecycle_manager', default_value='false',
+        description='Enable bond connection during node activation')
+
+
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
         'base_frame': tf_prefix + 'base_footprint',
@@ -47,10 +64,13 @@ def generate_launch_description():
         param_rewrites=param_substitutions,
         convert_types=True)
  
-    start_sync_slam_toolbox_node = Node(
+    start_sync_slam_toolbox_node = LifecycleNode(
         parameters=[
           configured_params,
-          {'use_sim_time': use_sim_time}
+          {
+            'use_sim_time': use_sim_time,
+            'use_lifecycle_manager': use_lifecycle_manager,
+          }
         ],
         package='slam_toolbox',
         executable='sync_slam_toolbox_node',
@@ -60,8 +80,34 @@ def generate_launch_description():
         output='screen'
       )
  
+    configure_event = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+            transition_id=Transition.TRANSITION_CONFIGURE
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
+
+    activate_event = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=start_sync_slam_toolbox_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
+                EmitEvent(event=ChangeState(
+                    lifecycle_node_matcher=matches_action(start_sync_slam_toolbox_node),
+                    transition_id=Transition.TRANSITION_ACTIVATE
+                ))
+            ]
+        ),
+        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    )
+
     ld = LaunchDescription()
  
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_lifecycle_manager)
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(start_sync_slam_toolbox_node)
