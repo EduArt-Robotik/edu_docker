d4402f746dfc9b8e482e86b16ff6bd8c20c277d9
David Kiesel <david.kiesel@eudart-robotik.com>
2025-02-24 15:34:16 +0100

    Adds support for robot namespace to launch file
    - remaps topics and frame_ids to the robot_namespace
    - adds rudimentary transformation (copied from rplidar)

--- sensor/tminipro/launch_content/ydlidar_launch.py
+++ sensor/tminipro/launch_content/ydlidar_launch.py
@@ -18,7 +18,7 @@ from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable
 from launch.actions import LogInfo
 
 import lifecycle_msgs.msg
@@ -26,27 +26,37 @@ import os
 
 
 def generate_launch_description():
+    robot_namespace = EnvironmentVariable('EDU_ROBOT_NAMESPACE', default_value="eduard")
     share_dir = get_package_share_directory('ydlidar_ros2_driver')
     parameter_file = LaunchConfiguration('params_file')
     node_name = 'ydlidar_ros2_driver_node'
 
     params_declare = DeclareLaunchArgument('params_file',
                                            default_value=os.path.join(
-                                               share_dir, 'params', 'TminiPro.yaml'),
+                                               share_dir, 'params', 'TminiPro.yaml'),       # TODO: Uses "original" param file, not the modified one with the different port and frame_id
                                            description='FPath to the ROS2 parameters file to use.')
 
     driver_node = LifecycleNode(package='ydlidar_ros2_driver',
-                                executable='ydlidar_ros2_driver_node',
-                                name='ydlidar_ros2_driver_node',
+                                executable=node_name,
+                                name=node_name,
                                 output='screen',
                                 emulate_tty=True,
-                                parameters=[parameter_file],
+                                parameters=[parameter_file,
+                                            # {"port": "/dev/tminipro"},        # Symlink doesn't work, uses default instead (/dev/ttyUSB0) 
+                                            {"frame_id": PathJoinSubstitution([robot_namespace, 'laser'])}
+                                            ],
                                 namespace='/',
+                                remappings=[
+                                    ('/scan', PathJoinSubstitution([robot_namespace, 'scan'])),
+                                    ('/point_cloud', PathJoinSubstitution([robot_namespace, 'point_cloud']))
+                                ]
                                 )
     tf2_node = Node(package='tf2_ros',
                     executable='static_transform_publisher',
                     name='static_tf_pub_laser',
-                    arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame'],
+                    arguments=['0.11', '0.0', '0.125','3.141592654', '0.0', '0.0', # TODO: Values only copied from rplidar - must be checked!
+                               PathJoinSubstitution([robot_namespace, 'base_link']),
+                               PathJoinSubstitution([robot_namespace, 'laser'])], 
                     )
 
     return LaunchDescription([
